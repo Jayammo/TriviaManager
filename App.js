0bc2f1a30737223539e5fcdd7a10e64a498b5011
Jayammo <josgood2@gmail.com>
2022-02-10 20:07:12 -0500

    Create QuestionPage Component and install native drawer, gesture handler and reanimated npm packages

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ListView from './components/ListView/ListView';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import AboutScreen from './components/_Screens/About/AboutScreen';
 import HomeScreen from './components/_Screens/Home/HomeScreen';
 import ProfileScreen from './components/_Screens/Profile/ProfileScreen';
-import AboutScreen from './components/_Screens/About/AboutScreen';
+import QuestionsPage from './components/_Screens/Questions/QuestionsPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -21,6 +20,7 @@ export default function App() {
 				/>
 				<Stack.Screen name='Profile' component={ProfileScreen} />
 				<Stack.Screen name='About' component={AboutScreen} />
+				<Stack.Screen name='Question' component={QuestionsPage} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
